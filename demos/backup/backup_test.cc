6d33e68d1f1e1359111f3ff7dccc8419668ca95e
Zaorang Yang <zaorangy@gmail.com>
2021-08-11 10:49:08 +0800

    update backup demo

--- demos/backup/backup_test.cc
+++ demos/backup/backup_test.cc
@@ -8,6 +8,7 @@
 #include "rocksdb/db.h"
 #include <iostream>
 #include <cassert>
+#include <vector>
 
 #include "rocksdb/db.h"
 #include "rocksdb/utilities/backupable_db.h"
@@ -26,7 +27,7 @@ int main() {
     options.file_checksum_gen_factory = GetFileChecksumGenCrc32cFactory();
                                                       
     DB* db;
-    Status s = DB::Open(options, "/tmp/rocksdb", &db);
+    Status s = DB::Open(options, "db", &db);
     assert(s.ok());
     std::string key = "key1";
     std::string value = "value1";
@@ -37,11 +38,36 @@ int main() {
     assert(s.ok());
 
     BackupEngine* backup_engine;
-    s = BackupEngine::Open(Env::Default(), BackupableDBOptions("/tmp/rocksdb_backup"), &backup_engine);
+    s = BackupEngine::Open(Env::Default(), BackupableDBOptions("db_backup"), &backup_engine);
     assert(s.ok());
     s = backup_engine->CreateNewBackup(db);
     assert(s.ok());
-    // db->Put(...); // make some more changes
-    // s = backup_engine->CreateNewBackup(db);
-    assert(s.ok());   
+    s = db->Put(rocksdb::WriteOptions(), "key2", "value2"); // make some more changes
+    assert(s.ok());
+    s = db->Flush(flush_opts);
+    assert(s.ok());
+    s = backup_engine->CreateNewBackup(db);
+    assert(s.ok());
+
+    std::vector<BackupInfo> backup_info;
+    backup_engine->GetBackupInfo(&backup_info);
+
+    // you can get IDs from backup_info if there are more than two
+    s = backup_engine->VerifyBackup(1 /* ID */);
+    assert(s.ok());
+    s = backup_engine->VerifyBackup(2 /* ID */);
+    assert(s.ok());
+    delete db;
+    delete backup_engine;
+
+    // Restore
+    BackupEngineReadOnly* backup_engine_ro;
+    s = BackupEngineReadOnly::Open(Env::Default(), BackupableDBOptions("db_backup"), &backup_engine_ro);
+    assert(s.ok());
+    s = backup_engine_ro->RestoreDBFromBackup(1, "db_backup_restore_1", "db_backup_restore_1");
+    assert(s.ok());
+    s = backup_engine_ro->RestoreDBFromBackup(2, "db_backup_restore_2", "db_backup_restore_2");
+    assert(s.ok());
+    delete backup_engine_ro;
+
 }
